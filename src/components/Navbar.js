e9211d6ef613821bb3ab6147c9882486cce5d7bb
Shalaka <111489373+shalaka-kapure@users.noreply.github.com>
2022-11-18 23:48:52 +0530

    fourth commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div>
       <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/news_daily">
+          <Link className="navbar-brand" to="/">
             News Daily
           </Link>
           <button
@@ -23,7 +23,7 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/news_daily">
+                <Link className="nav-link" aria-current="page" to="/">
                   Home
                 </Link>
               </li>
